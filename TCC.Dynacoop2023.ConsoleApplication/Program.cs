19403bedc5c8d842e4e429b674c95a7ceb933d1c
Caio Lourenço <lourencocaio228@gmail.com>
2023-03-02 16:32:39 -0300

    TCC

---
+++ TCC.Dynacoop2023.ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC.Dynacoop2023.ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
