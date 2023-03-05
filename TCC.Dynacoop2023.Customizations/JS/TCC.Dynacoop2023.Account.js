5565594640ac3c9d4b5280496fc05a5c66aec9d1
Caio Lourenço <lourencocaio228@gmail.com>
2023-03-05 17:39:08 -0300

    CNPJ E CPF

--- TCC.Dynacoop2023.Customizations/JS/TCC.Dynacoop2023.Account.js
+++ TCC.Dynacoop2023.Customizations/JS/TCC.Dynacoop2023.Account.js
@@ -10,7 +10,7 @@ Tcc.Account = {
     CNPJOnChange: function (executionContext) {
         var formContext = executionContext.getFormContext();
 
-        var cnpj = formContext.getAttribute("dcp_cnpj").getValue();
+        var cnpj = formContext.getAttribute("dyn1_cnpj").getValue();
 
         if (cnpj != null) {
             if (cnpj.length == 14) {
@@ -23,25 +23,25 @@ Tcc.Account = {
                     queryAccountId += " and accountid ne " + id;
                 }
 
-                Xrm.WebApi.online.retrieveMultipleRecords("account", "?$select=name&$filter=dcp_cnpj eq '" + formattedCNPJ + "'" + queryAccountId).then(
+                Xrm.WebApi.online.retrieveMultipleRecords("account", "?$select=name&$filter=dyn1_cnpj eq '" + formattedCNPJ + "'" + queryAccountId).then(
                     function success(results) {
                         if (results.entities.length == 0) {
-                            formContext.getAttribute("dcp_cnpj").setValue(formattedCNPJ);
+                            formContext.getAttribute("dyn1_cnpj").setValue(formattedCNPJ);
                         } else {
-                            formContext.getAttribute("dcp_cnpj").setValue("");
-                            AlfaPeople.Account.DynamicsAlert("O CNPJ já existe no sistema", "CNPJ duplicado!")
+                            formContext.getAttribute("dyn1_cnpj").setValue("");
+                            Tcc.Account.DynamicsAlert("O CNPJ já existe no sistema", "CNPJ duplicado!")
                         }
                     },
                     function (error) {
-                        AlfaPeople.Account.DynamicsAlert("Por favor contato o administrador", "Erro do sistema")
+                        Tcc.Account.DynamicsAlert("Por favor contato o administrador", "Erro do sistema")
                     }
                 );
             }
             else {
-                AlfaPeople.Account.DynamicsAlert("O CNPJ digitado não é valido", "CNPJ inválido!")
+                Tcc.Account.DynamicsAlert("O CNPJ digitado não é valido", "CNPJ inválido!")
             }
         } else {
-            AlfaPeople.Account.DynamicsAlert("Informe um valor para o CNPJ", "CNPJ incorreto!")
+            Tcc.Account.DynamicsAlert("Informe um valor para o CNPJ", "CNPJ incorreto!")
         }
     },
     DynamicsAlert: function (alertText, alertTitle) {
@@ -57,53 +57,5 @@ Tcc.Account = {
         };
 
         Xrm.Navigation.openAlertDialog(alertStrings, alertOptions);
-    },
-    FormControls: function () {
-        formContext.getControl("industrycode").setDisabled(true);
-
-        //SetRequiredLevel
-        formContext.getAttribute("primarycontactid").setRequiredLevel("required");
-
-        //SetVisible
-        formContext.getControl("fax").setVisible(false);
-    },
-    GetSet: function () {
-        //Get
-        //Número Inteiro
-        var numeroTotalOpp = formContext.getAttribute("dcp_nmr_total_opp").getValue();
-
-        //Texto
-        var nomeDaConta = formContext.getAttribute("name").getValue();
-
-        //Picklist
-        //Seguros == 20
-        var setor = formContext.getAttribute("industrycode").getValue();
-
-        //Money ou decimal
-        var valorTotalDeOpp = formContext.getAttribute("dcp_valor_total_opp").getValue();
-
-        //Lookup ou Consulta
-        var lookup = formContext.getAttribute("primarycontactid").getValue();
-
-        //Set
-        //Número Inteiro
-        formContext.getAttribute("dcp_nmr_total_opp").setValue(10);
-
-        //Text
-        formContext.getAttribute("name").setValue("Novo nome da Conta!");
-
-        //Picklist
-        formContext.getAttribute("industrycode").setValue(20);
-
-        //Money ou decimal
-        formContext.getAttribute("dcp_valor_total_opp").setValue(10.50);
-
-        //Lookup ou Consulta
-        var lookup = [];
-        lookup[0] = {};
-        lookup[0].name = "Gonçalves, Diego";
-        lookup[0].id = "cdd6a450-cb0c-ea11-a813-000d3a1b1223";
-        lookup[0].entityType = "contact";
-        formContext.getAttribute("primarycontactid").setValue(lookup);
     }
 }
---
+++ TCC.Dynacoop2023.Customizations/JS/Tcc.Dynacoop2023.Contact.js
@@ -0,0 +1,106 @@
+﻿if (typeof (Tcc) == "undefined") { Tcc = {} }
+if (typeof (Tcc.Contact) == "undefined") { Tcc.Contact = {} }
+
+
+Tcc.Contact = {
+    OnLoad: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+
+    },
+    CPFOnChange: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+
+        var cpf = formContext.getAttribute("dyn1_cpf").getValue();
+
+        if (cpf != null) {
+            if (cpf.length == 11) {
+                var formattedCPF = cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+                var id = Xrm.Page.data.entity.getId();
+
+                var queryContactId = "";
+
+                if (id.length > 0) {
+                    queryContactId += " and contactid ne " + id;
+                }
+
+                Xrm.WebApi.online.retrieveMultipleRecords("contact", "?$select=firstname&$filter=dyn1_cpf eq '" + formattedCPF + "'" + queryContactId).then(
+                    function success(results) {
+                        if (results.entities.length == 0) {
+                            formContext.getAttribute("dyn1_cpf").setValue(formattedCPF);
+                        } else {
+                            formContext.getAttribute("dyn1_cpf").setValue("");
+                            Tcc.Contact.DynamicsAlert("O CPF já existe no sistema", "CPF duplicado!")
+                        }
+                    },
+                    function (error) {
+                        Tcc.Contact.DynamicsAlert("Por favor contato o administrador", "Erro do sistema")
+                    }
+                );
+            }
+            else {
+                Tcc.Contact.DynamicsAlert("O CPF digitado não é valido", "CPF inválido!")
+            }
+        } else {
+            Tcc.Contact.DynamicsAlert("Informe um valor para o CPF", "CPF incorreto!")
+        }
+    },
+    ValidaCpf: function (executionContext)
+    {
+
+        ValidaCPF.valida = function () {
+            if (typeof this.cpfLimpo === 'undefined') return false;
+            if (this.cpfLimpo.length !== 11) return false;
+            if (this.isSequencia()) return false;
+
+            const cpfParcial = this.cpfLimpo.slice(0, -2);
+            const digito1 = this.criaDigito(cpfParcial);
+            const digito2 = this.criaDigito(cpfParcial + digito1);
+
+            const novoCpf = cpfParcial + digito1 + digito2;
+
+            return novoCpf === this.cpfLimpo;
+        };
+
+        ValidaCPF.prototype.criaDigito = function (cpfParcial) {
+            const cpfArray = Array.from(cpfParcial);
+
+            let regressivo = cpfArray.length + 1;
+            const total = cpfArray.reduce((ac, val) => {
+                ac += (regressivo * Number(val));
+                regressivo--;
+                return ac;
+            }, 0);
+
+            const digito = 11 - (total % 11);
+            return digito > 9 ? '0' : String(digito);
+        };
+
+        ValidaCPF.prototype.isSequencia = function () {
+            const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+            return sequencia === this.cpfLimpo;
+        };
+
+        const cpf = new ValidaCPF('705.484.450-52');
+
+        if (cpf.valida()) {
+            console.log('Cpf válido');
+        }
+        else {
+            console.log('Cpf inválido');
+        }
+    },
+    DynamicsAlert: function (alertText, alertTitle) {
+        var alertStrings = {
+            confirmButtonLabel: "OK",
+            text: alertText,
+            title: alertTitle
+        };
+
+        var alertOptions = {
+            height: 120,
+            width: 200
+        };
+
+        Xrm.Navigation.openAlertDialog(alertStrings, alertOptions);
+    }
+}
